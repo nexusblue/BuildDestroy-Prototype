92f005e5abbe7acdec28714a4db8bf7d8a8900db
Tony Nguyen <blue28275@yahoo.com>
2019-04-22 10:44:48 -0500

    Puffing update
    Puffing works now, just use the settings I have in the inspector.
    
    The puff is in the prefabs folder

--- BuildDestroy Prototype/Assets/Scripts/Tony's Scripts/PlayerController.cs
+++ BuildDestroy Prototype/Assets/Scripts/Tony's Scripts/PlayerController.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-	public GameObject puff;
+	public GameObject puffPrefab;
+	GameObject puffInstance;
 	public Transform puffSpawnPoint;
+
+	public Slider puffGauge;
+	public Text puffText;
 
 	public float maxPuffRadius;
 	public float maxPuffSpeed;
@@ -11,35 +16,42 @@ public class PlayerController : MonoBehaviour
 
 	float chargeTimer;
 	float chargeValue;
+	public float chargeSpeed;
 
-	public float gaugeSensitivity;
+	void Start()
+	{
+		puffGauge.gameObject.SetActive(false);
+	}
 
 	void Update()
 	{
 		if(Input.GetKey(KeyCode.F))
 		{
 			ChargeTiming();
+			puffGauge.gameObject.SetActive(true);
+			puffText.gameObject.SetActive(false);
 		}
 		if(Input.GetKeyUp(KeyCode.F))
 		{
 			chargeTimer = 0f;
 
-			puff.GetComponent<SphereCollider>().radius = chargeValue * maxPuffRadius;
-			puff.GetComponent<Rigidbody>().mass = chargeValue * maxPuffMass;
-			puff.GetComponent<Rigidbody>().velocity = Vector3.forward * chargeValue * maxPuffSpeed;
+			puffInstance = Instantiate(puffPrefab, puffSpawnPoint.position, puffSpawnPoint.localRotation);
 
-			Instantiate(puff, puffSpawnPoint.position, puffSpawnPoint.localRotation);
+			puffInstance.GetComponent<SphereCollider>().radius = chargeValue * maxPuffRadius;
+			puffInstance.GetComponent<Rigidbody>().mass = chargeValue * maxPuffMass;
+			puffInstance.GetComponent<Rigidbody>().velocity = puffSpawnPoint.transform.forward * chargeValue * maxPuffSpeed;
 		}
 	}
 
 	void ChargeTiming()
 	{
 		chargeTimer += Time.deltaTime;
-		chargeValue = ChargeFunction(chargeTimer);
+		chargeValue = ChargeFunction(chargeTimer * chargeSpeed);
+		puffGauge.value = chargeValue;
 	}
 
 	public float ChargeFunction(float x)
 	{
-		return Mathf.Pow(Mathf.Sin(x * Mathf.PI), gaugeSensitivity);
+		return Mathf.PingPong(x, 1);
 	}
 }
