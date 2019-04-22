f661d92cc45b0b76cb999b43fa688dbd212c7f7d
Tony Nguyen <blue28275@yahoo.com>
2019-04-22 00:29:26 -0500

    Puff Function
    +temp. player controller
    +puff function
    
    needs work:
    -instantiating puff sphere correctly with speed
    -actual puff will have mesh renderer turned off

---
+++ BuildDestroy Prototype/Assets/Scripts/Tony's Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public GameObject puff;
+	public Transform puffSpawnPoint;
+
+	public float maxPuffRadius;
+	public float maxPuffSpeed;
+	public float maxPuffMass;
+
+	float chargeTimer;
+	float chargeValue;
+
+	public float gaugeSensitivity;
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.F))
+		{
+			ChargeTiming();
+		}
+		if(Input.GetKeyUp(KeyCode.F))
+		{
+			chargeTimer = 0f;
+
+			puff.GetComponent<SphereCollider>().radius = chargeValue * maxPuffRadius;
+			puff.GetComponent<Rigidbody>().mass = chargeValue * maxPuffMass;
+			puff.GetComponent<Rigidbody>().velocity = Vector3.forward * chargeValue * maxPuffSpeed;
+
+			Instantiate(puff, puffSpawnPoint.position, puffSpawnPoint.localRotation);
+		}
+	}
+
+	void ChargeTiming()
+	{
+		chargeTimer += Time.deltaTime;
+		chargeValue = ChargeFunction(chargeTimer);
+	}
+
+	public float ChargeFunction(float x)
+	{
+		return Mathf.Pow(Mathf.Sin(x * Mathf.PI), gaugeSensitivity);
+	}
+}
