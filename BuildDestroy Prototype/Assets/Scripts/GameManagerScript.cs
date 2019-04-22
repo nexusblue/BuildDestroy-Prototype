eea37bfc51c8e1609e832cfc53e70e9a7683e61c
Peyton Breech <peyton.breech16@gmail.com>
2019-04-22 13:35:46 -0500

    Final changes lol

---
+++ BuildDestroy Prototype/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public GameObject builder;
+    public GameObject wolf;
+    public Text puffText;
+    public Slider puffGauge;
+    public Canvas builderCanvas;
+    public AudioSource builderMusic;
+    public AudioSource wolfMusic;
+
+
+    private void Start()
+    {
+        wolf.gameObject.SetActive(false);
+        puffText.gameObject.SetActive(false);
+        puffGauge.gameObject.SetActive(false);
+        wolfMusic.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            builder.gameObject.SetActive(false);
+            wolf.gameObject.SetActive(true);
+            puffText.gameObject.SetActive(true);
+            puffGauge.gameObject.SetActive(true);
+            builderCanvas.gameObject.SetActive(false);
+            wolfMusic.gameObject.SetActive(true);
+            builderMusic.gameObject.SetActive(false);
+        }
+    }
+}
